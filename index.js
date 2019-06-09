360e5b1bf0449b6a85d90c9fa4ea66bc26ad3973
leonimm <leon.seemann@gmail.com>
2019-06-09 10:00:10 +0200

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ import './style.css';
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+console.log("test");
+console.log("blup");
+
+var chipTemplate = `<div class="chip"></div>`
+var board = document.getElementById("board")
+console.log(board)
+var content = "";
+//var i=1;
+//while (i<=10)
+//{
+//  content = content + chipTemplate;
+//  i = i + 1;
+//}
+board.innerHTML = content;
